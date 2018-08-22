b2f17d7e8f78af4688267582fb3672ff56ca6626
Pedro Mercado <zafirapeter3@gmail.com>
2018-08-22 14:03:12 -0400

    Inicio de lista de comandos

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Comando } from './model/comando';
 
 @Component({
   selector: 'my-app',
@@ -7,11 +8,20 @@ import { Component } from '@angular/core';
   `],
   template: `
     <div class="jumbotron text-center">
-      <h1>The App Lives!</h1>
+      <h1>GIT!</h1>
       <p>{{ message }}</p>
     </div>
   `
 })
 export class AppComponent {
   message = 'This is the sample message.';
+  private listaComandos: Comando[];
+
+  constructor(){
+    this.listaComandos = [
+      new Comando("git status","Muestra el estado de los archivos comparados con la version HEAD")
+    ];
+  }
+
+
 }
---
+++ app/model/comando.ts
@@ -0,0 +1,9 @@
+export class Comando {
+    nombre: string;
+    descripcion: string;
+
+    constructor(nombre, descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+}
