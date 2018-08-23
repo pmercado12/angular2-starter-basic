a4692d4bd4d6420e59494d18af6f5c6234907b7f
Pedro Mercado <zafirapeter3@gmail.com>
2018-08-22 23:20:20 -0400

    Se crea componente para ser utilizado en otros módulos

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ComandosGitComponent } from './components/comandos-git/comandos-git.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  imports: [BrowserModule],
+  declarations: [AppComponent, ComandosGitComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
